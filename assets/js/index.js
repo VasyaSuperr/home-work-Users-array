447c65f6c9a9f77d4fb75a75e8fe908edef4276f
VasyaSuperr <yenakii.vasyl@gmail.com>
2024-05-31 12:20:31 +0300

    Task 1, 2, 3, 4

--- assets/js/index.js
+++ assets/js/index.js
@@ -25,6 +25,33 @@ for (let i = 0; i < 10; i++) {
   users.push(user);
 }
 
-console.log(users[0].getFullName());
+console.log(users);
 
-//console.log(users);
+console.log("Method getFullName: ");
+console.log("Full name for 1st user:", users[1].getFullName());
+
+console.log("Task 1");
+const notSubscribedUsers = users.filter((user) => !user.isSubscribed);
+console.log("Not subscribed users: ", notSubscribedUsers);
+
+console.log("Task 2");
+const listFullNameUsers = users.map((user) => user.getFullName());
+console.log("List full name users: ", listFullNameUsers);
+
+console.log("Task 3");
+const femaleSchoolAgeFullName = users
+  .filter((user) => !user.isMale && user.age >= 6 && user.age <= 18)
+  .map((user) => user.getFullName());
+console.log("Female school age full name: ", femaleSchoolAgeFullName);
+
+console.log("Task 4");
+const indexOfDeleted = users.findIndex(
+  (user) => user.email === "useremail5@gmail.com"
+);
+
+if (indexOfDeleted !== -1) {
+  users.slice(indexOfDeleted, 1);
+  console.log(`User with useremail${indexOfDeleted}@gmail.com was deleted`);
+} else {
+  console.log(`User with useremail${indexOfDeleted}@gmail.com doesn't exist`);
+}
